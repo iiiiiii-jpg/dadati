2b3334f2846bffbcacfbff556fc7a110053ac172
iiiiiii-jpg <68292284+iiiiiii-jpg@users.noreply.github.com>
2024-04-07 20:28:13 +0530

    Add files via upload

--- dadati/dadati.abd2.js
+++ dadati/dadati.abd2.js
@@ -58,7 +58,7 @@ function handleFiles() {
 let now_playing = document.querySelector(".now-playing");
 let track_art = document.querySelector(".track-art");
 let track_name = document.querySelector(".track-name");
-let track_artist = document.querySelector(".track-artist");
+//let track_artist = document.querySelector(".track-artist");
 
 let playpause_btn = document.querySelector(".playpause-track");
 let next_btn = document.querySelector(".next-track");
@@ -109,7 +109,7 @@ function loadTrack(track_index) {
 
   //track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
   track_name.textContent = track_list[track_index].name;
-  track_artist.textContent = track_list[track_index].artist;
+  //track_artist.textContent = track_list[track_index].artist;
   now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
 
   updateTimer = setInterval(seekUpdate, 1000);
